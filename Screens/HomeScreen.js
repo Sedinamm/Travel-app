7c20a2e5142f2cde629ed68405692dc2acd590d0
Sedinam <sedinam436@gmail.com>
2022-10-12 10:27:17 -0200

    added an image and icon to the top header

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,12 +1,47 @@
-import { View, Text, } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import React from "react";
+// import { SafeAreaView } from 'react-native-safe-area-context'
+import { Ionicons } from "@expo/vector-icons";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View
+        style={{
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          marginHorizontal: 20,
+        }}
+      >
+        {/* Top Header Navigation */}
+        {/* Hamburger Icon */}
+        <TouchableOpacity>
+          <Ionicons
+            name="menu"
+            size={30}
+            color="black"
+          />
+        </TouchableOpacity>
+        {/* Text */}
+        <Text style={{fontSize:25, fontWeight:"bold"}}>Discover</Text>
 
-export default HomeScreen
+        {/* Profile Image */}
+        <TouchableOpacity>
+          <Image
+            style={{ height: 40, width: 40, borderRadius: 50 }}
+            source={require("../Images/profile.jpg")}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
